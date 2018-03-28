94b4c00da8ff4b03329207aee6c305d81a07f29e
Daniel Arnett <arnett.daniel@gmail.com>
2018-03-27 23:07:44 -0400

    Finished 3.1

--- hw3/code/SimpleEnvironment.py
+++ hw3/code/SimpleEnvironment.py
@@ -3,7 +3,7 @@ import pylab as pl
 from DiscreteEnvironment import DiscreteEnvironment
 
 class SimpleEnvironment(object):
-    
+
     def __init__(self, herb, resolution):
         self.robot = herb.robot
         self.lower_limits = [-5., -5.]
@@ -14,9 +14,9 @@ class SimpleEnvironment(object):
         table = self.robot.GetEnv().ReadKinBodyXMLFile('models/objects/table.kinbody.xml')
         self.robot.GetEnv().Add(table)
 
-        table_pose = numpy.array([[ 0, 0, -1, 1.5], 
-                                  [-1, 0,  0, 0], 
-                                  [ 0, 1,  0, 0], 
+        table_pose = numpy.array([[ 0, 0, -1, 1.5],
+                                  [-1, 0,  0, 0],
+                                  [ 0, 1,  0, 0],
                                   [ 0, 0,  0, 1]])
         table.SetTransform(table_pose)
 
@@ -28,28 +28,96 @@ class SimpleEnvironment(object):
         #  up the configuration associated with the particular node_id
         #  and return a list of node_ids that represent the neighboring
         #  nodes
-        
+        # Change to True to generate all 8 points instead of just 4
+        all_eight_points = False
+        # Get the limits
+        top_limit = self.upper_limits[1]
+        bottom_limit = self.lower_limits[1]
+        left_limit = self.lower_limits[0]
+        right_limit = self.upper_limits[0]
+        # Coordinates of the node
+        coord = self.discrete_env.NodeIdToConfiguration(node_id)
+        # X or Y distance to move in a single step
+        delta = self.discrete_env.resolution
+        # The four coordinates to consider moving to
+        top_coord    = [coord[0], coord[1] + delta]
+        bottom_coord = [coord[0], coord[1] - delta]
+        left_coord   = [coord[0] - delta, coord[1]]
+        right_coord  = [coord[0] + delta, coord[1]]
+        # The other four coordinates you can optionally consider
+        if all_eight_points:
+            top_left_coord     = [coord[0] - delta, coord[1] + delta]
+            top_right_coord    = [coord[0] + delta, coord[1] + delta]
+            bottom_left_coord  = [coord[0] - delta, coord[1] - delta]
+            bottom_right_coord = [coord[0] + delta, coord[1] - delta]
+        # Get the IDs of the four coordinates
+        top_id          = self.discrete_env.ConfigurationToNodeId(top_coord)
+        bottom_id       = self.discrete_env.ConfigurationToNodeId(bottom_coord)
+        left_id         = self.discrete_env.ConfigurationToNodeId(left_coord)
+        right_id        = self.discrete_env.ConfigurationToNodeId(right_coord)
+        # Get the IDs of the optional four
+        if all_eight_points:
+            top_left_id     = self.discrete_env.ConfigurationToNodeId(top_left_coord)
+            top_right_id    = self.discrete_env.ConfigurationToNodeId(top_right_coord)
+            bottom_left_id  = self.discrete_env.ConfigurationToNodeId(bottom_left_coord)
+            bottom_right_id = self.discrete_env.ConfigurationToNodeId(bottom_right_coord)
+        # If it's below the top limit, add the top node
+        if top_coord[1] < top_limit:
+            successors.append(top_id)
+            # Check and optionally add the two optional top nodes
+            if all_eight_points:
+                if left_coord[0] > left_limit:
+                    successors.append(top_left_id)
+                if right_coord[0] < right_limit:
+                    successors.append(top_right_id)
+        # If it's to the right of the left limit add the left node
+        if left_coord[0] > left_limit:
+            successors.append(left_id)
+        # If it's to the left of the right limit add the right node
+        if right_coord[0] < right_limit:
+            successors.append(right_id)
+        # If it's above the bottom limit add the bottom node
+        if bottom_coord[1] > bottom_limit:
+            successors.append(bottom_id)
+            # Check and optionally add the two optional bottom nodes
+            if all_eight_points:
+                if left_coord[0] > left_limit:
+                    successors.append(bottom_left_id)
+                if right_coord[0] < right_limit:
+                    successors.append(bottom_right_id)
+        # variable = raw_input('Any key to continue: ')
+
         return successors
 
     def ComputeDistance(self, start_id, end_id):
-
-        dist = 0
-
-        # TODO: Here you will implement a function that 
+        # TODO: Here you will implement a function that
         # computes the distance between the configurations given
         # by the two node ids
 
-        return dist
+        # Calculate distance using pythagorean theorem, may need abstracted for higher dimensional spaces
+        # a^2 = b^2 + c^2
+        startX = self.discrete_env.NodeIdToConfiguration(start_id)[0]
+        startY = self.discrete_env.NodeIdToConfiguration(start_id)[1]
+        endX = self.discrete_env.NodeIdToConfiguration(end_id)[0]
+        endY = self.discrete_env.NodeIdToConfiguration(end_id)[1]
+        # a^2
+        deltaXSquared = (endX - startX) * (endX - startX)
+        # b^2
+        deltaYSquared = (endY - startY) * (endY - startY)
+
+        # Square root of a^2 + b^2 gives us the distancd
+        return numpy.sqrt(deltaXSquared + deltaYSquared)
 
     def ComputeHeuristicCost(self, start_id, goal_id):
-        
-        cost = 0
-
-        # TODO: Here you will implement a function that 
+        # TODO: Here you will implement a function that
         # computes the heuristic cost between the configurations
         # given by the two node ids
 
-        return cost
+        cost_sum = 0;
+        for x in range(self.discrete_env.dimension):
+            cost_sum = cost_sum + abs(self.discrete_env.NodeIdToConfiguration(start_id)[x] - self.discrete_env.NodeIdToConfiguration(goal_id)[x])
+
+        return cost_sum
 
     def InitializePlot(self, goal_config):
         self.fig = pl.figure()
@@ -72,15 +140,13 @@ class SimpleEnvironment(object):
                      bb.pos()[1] + bb.extents()[1],
                      bb.pos()[1] + bb.extents()[1],
                      bb.pos()[1] - bb.extents()[1]], 'r')
-                    
-                     
+
+
         pl.ion()
         pl.show()
-        
+
     def PlotEdge(self, sconfig, econfig):
         pl.plot([sconfig[0], econfig[0]],
                 [sconfig[1], econfig[1]],
                 'k.-', linewidth=2.5)
         pl.draw()
-
-        
