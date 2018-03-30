7118cbf54ad8100747b9126d6f23cb22bfb0f6be
Daniel Arnett <arnett.daniel@gmail.com>
2018-03-29 21:59:34 -0400

    Discrete merge 1

--- hw3/code/DiscreteEnvironment.py
+++ hw3/code/DiscreteEnvironment.py
@@ -73,58 +73,25 @@ class DiscreteEnvironment(object):
         # This function maps a grid coordinate to the associated
         # node id
         node_id = 0
-        # c = coord
-        for idx in range(self.dimension):
-            dim = coord[idx]
-            for i in range(idx+1,self.dimension):
-                dim = dim*self.num_cells[i]
-            node_id = node_id + dim
-
+        for i in range(self.dimension):
+        	tmp = 1
+        	for j in range(i+1, self.dimension):
+        		tmp *= self.num_cells[j]
+        	node_id += coord[i] * tmp
+        # node_id = numpy.ravel_multi_index(coord, self.num_cells, order='F')
         return node_id
-
 
     def NodeIdToGridCoord(self, node_id):
 
         # TODO:
         # This function maps a node id to the associated
         # grid coordinate
-        dim = 1
-        for i in range(1,self.dimension):
-            dim = dim*self.num_cells[i]
-
         coord = [0] * self.dimension
-        index = node_id
-        denom_dim = dim
-        numer_dim = dim
-
-        #Peter doulble check
-        #index2 = node_id
-        #dim2 = dim
-        #coord2 = [0]*self.dimension
-
-        for idx in range(self.dimension):
-            index = node_id
-            numer_dim = dim
-
-            for j in range(0,idx):
-               index = index - coord[j]*numer_dim
-               numer_dim = numer_dim / self.num_cells[j + 1]
-
-            # if idx == self.dimension - 1 :
-            #     coord[idx] = index % self.num_cells[0]
-            # else:
-            coord[idx] = numpy.floor(index / denom_dim)
-            if idx < self.dimension - 1 :
-                denom_dim = denom_dim / self.num_cells[idx+1]
-            else:
-                denom_dim = 1
-            #Peter, just for double check
-            #coord2[idx] = numpy.floor(index2 / dim2)
-            #index2 = index2 - coord2[idx]*dim2
-            #if(idx < (self.dimension -1)):
-            #    dim2 = dim2 / self.num_cells[idx+1]
-            #else:
-            #    dim2 = 1
-            #assert(coord2[idx] == coord[idx])
-        # c = coord
+        for i in range(self.dimension):
+            tmp = 1
+            for j in range(i+1, self.dimension):
+                tmp *= self.num_cells[j]
+            coord[i] = int(node_id // tmp)
+            node_id -= coord[i] * tmp
+        # coord = numpy.unravel_index(node_id, self.num_cells, order='F')
         return coord
